929edbe17705e6e4ae2c27b3f4493d2a183cd410
Ahmad Khumaidi <khumaidiahmad734@gmail.com>
2024-07-04 11:24:21 +0700

    Update styles, add new images, and modify main content component

--- frontend/src/Components/MainContent.js
+++ frontend/src/Components/MainContent.js
@@ -2,9 +2,9 @@ import React from "react";
 import "../App.css";
 import sampleImage from "../images/Group.jpg";
 import apimage from "../images/bakso.jpg";
-import search from "../images/search.jpg";
-import location from "../images/location.jpg";
-import monetization from "../images/monetization.jpg";
+import search from "../images/search.png";
+import location from "../images/location.png";
+import monetization from "../images/monetization.png";
 import batik from "../images/batik.jpg";
 import logo from "../images/logo.jpg";
 
@@ -55,17 +55,51 @@ function MainContent() {
             <div className="kain">
               <img src={search} alt="search" />
               <h3>Cari produk</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p>
+                Temukan produk yang Anda butuhkan dengan cepat dan mudah
+                menggunakan fitur "Cari Produk" kami. Dengan berbagai kategori
+                dan filter yang komprehensif, Anda dapat mempersempit pencarian
+                berdasarkan merek, harga, ulasan pelanggan, dan banyak lagi.
+                Baik Anda mencari pakaian, Makanan khas, Oleh-oleh, atau
+                kebutuhan sehari-hari, kami menyediakan berbagai pilihan produk
+                dari berbagai penjual terpercaya. Jelajahi dan bandingkan produk
+                dengan informasi yang lengkap dan jelas untuk membuat keputusan
+                belanja yang lebih baik. Mulai pencarian Anda sekarang dan
+                nikmati pengalaman berbelanja yang menyenangkan dan efisien
+                hanya di platform kami.
+              </p>
             </div>
             <div className="kain">
               <img src={location} alt="location" />
-              <h3>isi alamat kamu</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3>Isi Alamat Kamu</h3>
+              <p>
+                Pastikan pesanan Anda sampai dengan tepat waktu dan di tempat
+                yang benar dengan mengisi informasi alamat yang akurat. Fitur
+                "Isi Alamat Kamu" memungkinkan Anda memasukkan detail alamat
+                lengkap termasuk nama jalan, nomor rumah, kode pos, kota, dan
+                provinsi. Anda juga dapat menambahkan instruksi khusus untuk
+                pengiriman guna memastikan paket Anda diterima dengan baik.
+                Dengan menyimpan alamat Anda di akun, proses checkout menjadi
+                lebih cepat dan mudah untuk pembelian berikutnya. Isi alamat
+                kamu sekarang dan nikmati pengalaman berbelanja yang lebih
+                lancar dan nyaman di platform kami.
+              </p>
             </div>
             <div className="kain">
               <img src={monetization} alt="gambar" />
               <h3>Konfirmasi pembayaran</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p>
+                Segera konfirmasikan pembayaran Anda untuk memproses pesanan
+                dengan cepat dan tepat. Fitur "Konfirmasi Pembayaran" memudahkan
+                Anda dalam menginformasikan bahwa pembayaran telah dilakukan.
+                Cukup unggah bukti transfer atau isi detail transaksi seperti
+                nomor rekening, jumlah pembayaran, dan tanggal transaksi. Dengan
+                konfirmasi yang cepat, pesanan Anda akan segera diproses dan
+                dikirimkan. Pastikan semua informasi yang Anda berikan akurat
+                untuk menghindari keterlambatan. Nikmati pengalaman belanja yang
+                aman dan nyaman dengan sistem konfirmasi pembayaran kami yang
+                efisien.
+              </p>
             </div>
           </div>
         </section>
@@ -126,6 +160,9 @@ function MainContent() {
             <p>Hubungi kami</p>
           </div>
         </section>
+        <footer className="footer">
+          <p className="footer-text">Copyright 2024 Boengkoes®️</p>
+        </footer>
       </main>
     </div>
   );
